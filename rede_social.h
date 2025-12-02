92455725ec526c5ad72230a4f7f47e6a1dc90fe8
Gabriel leite Silva <gabrieleitesilvax@gmail.com>
2025-12-02 17:37:59 -0300

    Update rede_social.h

--- rede_social.h
+++ rede_social.h
@@ -1,4 +1,18 @@
 #ifndef REDE_SOCIAL_H
 #define REDE_SOCIAL_H
+
 #include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+
+typedef struct
+{
+    int id, idade;
+    char nome[100];
+
+    float influencia;
+    float influenciado;
+};
+
 #endif
