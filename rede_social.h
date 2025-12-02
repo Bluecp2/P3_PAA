19d22fcf188ca62f2438ff60ab1a0b3a45b58a80
Gabriel leite Silva <gabrieleitesilvax@gmail.com>
2025-12-02 18:02:59 -0300

    Update rede_social.h

--- rede_social.h
+++ rede_social.h
@@ -6,13 +6,25 @@
 #include<string.h>
 
 
-typedef struct
-{
+typedef struct{
     int id, idade;
     char nome[100];
 
     float influencia;
-    float influenciado;
-};
+    float influenciado;   
+}Pessoa;
+
+typedef struct {
+    int conexao;// quem influencia
+    int destino;//quem e influenciado
+    float peso;
+}Conexao;
+
+typedef struct{
+    int maxPessoas, qtdPessoas;
+    int maxConexoes, qtdConexoes;
+    Pessoa *pessoas;
+    Conexao *matriz;
+} RedeSocial;
 
 #endif
